aa650eab66edbfb1af160478984f8f7b717ddc35
Stefan Cosma <stefcosma@gmail.com>
2015-04-21 07:02:39 +0000

    Changed file / folder structure

--- gruntfile.js
+++ gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
                     removeComments: true
                 },
                 files: {
-                    'index.html': 'core/template/main.html'
+                    'build/index.html': 'core/template/main.html'
                 }
             }
         },
@@ -20,7 +20,7 @@ module.exports = function(grunt) {
                     sourcemap: 'none'
                 },
                 files: {
-                    'core/dest/css/screen.min.css' : 'core/sass/screen.scss'
+                    'build/assets/css/screen.min.css' : 'core/sass/screen.scss'
                 }
             }
         },
@@ -30,26 +30,35 @@ module.exports = function(grunt) {
                     join: true
                 },
                 files: {
-                    'core/dest/js/app.min.js': ['core/coffee/*.coffee']
+                    'build/assets/js/app.min.js': ['core/coffee/*.coffee']
                 }
             }  
         },
         uglify: {
             js: {
                 files: {
-                    'core/dest/js/app.min.js': ['core/dest/js/app.min.js']
+                    'build/assets/js/app.min.js': ['build/assets/js/app.min.js']
                 }
             }
         },
+        copy: {
+            main: {
+                expand: true,
+                cwd: 'core/assets/',
+                src: '**',
+                dest: 'build/assets/'
+            },
+        },
         watch: {
             files: ['core/template/*', 'core/sass/*', 'core/coffee/*'],
-            tasks: ['htmlmin', 'sass', 'coffee', 'uglify']
+            tasks: ['htmlmin', 'sass', 'coffee', 'uglify', 'copy']
         }
     });
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-coffee');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['htmlmin', 'sass', 'coffee', 'uglify']);
+    grunt.registerTask('default', ['htmlmin', 'sass', 'coffee', 'uglify', 'copy']);
 };
